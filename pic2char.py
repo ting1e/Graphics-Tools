7083690f5335fb267ba8008429ef517afb56c410
SlyLi <slylicn@qq.com>
2018-05-07 17:36:15 +0800

    update

--- pic2char.py
+++ pic2char.py
@@ -1,8 +1,12 @@
-from PIL import Image,ImageSequence
+# -*- coding: utf-8 -*-
+from PIL import Image,ImageSequence,ImageDraw,ImageFont
+import os
 import argparse
 
-ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!;:,\"^`'.")
+IMG="wjz.gif"
 
+
+ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!;:,\"^`'.")
 
 def get_char(r,g,b,alpha = 256):
     if alpha == 0:
@@ -14,26 +18,53 @@ def get_char(r,g,b,alpha = 256):
     return ascii_char[int(gray/unit)]
 
 
-
-
-IMG="wjz.gif"
-WIDTH=150
-HEIGHT=60
-
-
 if __name__ == '__main__':
 
-    with Image.open(IMG) as img:
-        for f in ImageSequence.Iterator(img):
-            im=f.copy()
-            im = im.resize((WIDTH,HEIGHT), Image.NEAREST)
-            txt=""
+    if IMG.split('.')[-1]!='gif':
+        with Image.open(IMG) as img:
+
+            WIDTH,HEIGHT=img.size
+            WIDTH=int(WIDTH/8)
+            HEIGHT=int(HEIGHT/10)
+            img = img.resize((WIDTH, HEIGHT), Image.NEAREST)
+
+            im = Image.new("RGB", (WIDTH * 6, HEIGHT * 8), (255, 255, 255))
+            dr = ImageDraw.Draw(im)
+            font = ImageFont.load_default().font
+
             for i in range(HEIGHT):
+                txt = ""
                 for j in range(WIDTH):
-                    txt += get_char(*im.convert('RGB').getpixel((j,i)))
-                txt += '\n'
-            print(txt)
+                    txt += get_char(*img.convert('RGB').getpixel((j, i)))
+                dr.text((0, 8 * i), txt, font=font, fill="#000000")
+            im.save('_'+IMG)
 
 
+    else:
+        with Image.open(IMG) as img:
 
+            WIDTH,HEIGHT=img.size
+            WIDTH = int(WIDTH / 4)
+            HEIGHT = int(HEIGHT / 5)
+
+            images = []
+
+            for _im in ImageSequence.Iterator(img):
+                #    images.append(_im.copy())
+                _im = _im.resize((WIDTH, HEIGHT), Image.NEAREST)
+
+                im = Image.new("RGB", (WIDTH * 6, HEIGHT * 8), (255, 255, 255))
+                dr = ImageDraw.Draw(im)
+                font = ImageFont.load_default().font
+
+                for i in range(HEIGHT):
+                    txt = ""
+                    for j in range(WIDTH):
+                        txt += get_char(*_im.convert('RGB').getpixel((j, i)))
+                    dr.text((0, 8 * i), txt, font=font, fill="#000000")
+
+                images.append(im.copy())
+
+        images[0].save("_"+IMG, save_all=True, append_images=images[1:])
     
+
